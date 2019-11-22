90bae1663a75609522d77b0ebebd5279a278c7b6
Rupture13 <marco.nieuwenhuizen@student.fontys.nl>
2019-11-22 15:59:12 +0100

    Basic Dragging added

--- Assets/Draggable.cs
+++ Assets/Draggable.cs
@@ -10,4 +10,25 @@ public abstract class Draggable : MonoBehaviour
     private bool isDragged;
     private int dragId;
 
+    private TargetJoint2D dragJoint;
+
+    private void Awake()
+    {
+        dragJoint = gameObject.GetComponent<TargetJoint2D>();
+    }
+
+    private void OnMouseDown()
+    {
+        dragJoint.enabled = true;
+    }
+
+    private void OnMouseDrag()
+    {
+        dragJoint.target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseUp()
+    {
+        dragJoint.enabled = false;
+    }
 }
